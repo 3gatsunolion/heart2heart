35410cf972c2101303bf130a9ae1f555470d0c12
Susan Wu <timetoonlineshop@gmail.com>
2022-07-07 16:44:12 -0600

    Update bot activity immediately

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,15 @@
 import { Client } from 'discord.js';
 
+async function setBotActivity(bot: Client) {
+    bot.user!.setActivity(`${process.env.PREFIX || '?'}help | 💕`, { type: 'PLAYING' })
+}
+
 export default async function onReady(bot: Client) {
     console.info(`${bot.user!.username} is connected.`);
     // https://github.com/discord/discord-api-docs/issues/834 --> presence disappears every ~2 hours,
     // so reset every 1 hour
+    setBotActivity(bot)
     setInterval(() => {
-        bot.user!.setActivity(`${process.env.PREFIX || '?'}help | 💕`, { type: 'PLAYING' })
+        setBotActivity(bot)
     }, 1000 * 60 * 60)
 }
