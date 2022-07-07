f95925919d04f08f39005264fa2112ea5d2f5570
Susan Wu <timetoonlineshop@gmail.com>
2022-07-07 16:39:30 -0600

    Fixed Discord Bot Activity Disappearing Issue

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,5 +2,9 @@ import { Client } from 'discord.js';
 
 export default async function onReady(bot: Client) {
     console.info(`${bot.user!.username} is connected.`);
-    bot.user!.setActivity(`${process.env.PREFIX || '?'}help | 💕`, { type: 'PLAYING' })
+    // https://github.com/discord/discord-api-docs/issues/834 --> presence disappears every ~2 hours,
+    // so reset every 1 hour
+    setInterval(() => {
+        bot.user!.setActivity(`${process.env.PREFIX || '?'}help | 💕`, { type: 'PLAYING' })
+    }, 1000 * 60 * 60)
 }
